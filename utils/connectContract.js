bf5feb18a0eb2fe8cd8b58b00932ca26886909d3
Silvia Ocegueda <margarita.ocegueda@gmail.com>
2022-08-21 10:26:09 -0600

    Front-end connects to wallet and creates events

---
+++ utils/connectContract.js
@@ -0,0 +1,25 @@
+import { abiJSON } from "./Web3RSVP.json";
+import { ethers } from "ethers";
+
+function connectContract() {
+  const contractAddress = "0xe9b2F5930B2C55468918bE6Aa192D8b5df93D58f";
+  const contractABI = abiJSON.abi;
+  let rsvpContract;
+  try {
+    const { ethereum } = window;
+
+    if (ethereum) {
+      //checking for eth object in the window
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      rsvpContract = new ethers.Contract(contractAddress, contractABI, signer); //instantiating new connection to the contract
+    } else {
+      console.log("Ethereum object doesn't exist!");
+    }
+  } catch (error) {
+    console.log("ERROR:", error);
+  }
+  return rsvpContract;
+}
+
+export default connectContract;
